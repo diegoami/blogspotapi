42e7598f4a38a5093960b45b70e0e017e55192e5
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-10 20:51:39 +0100

    blogsptoaipwq

--- blogspotapi/blog_repository.py
+++ blogspotapi/blog_repository.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 from .blog_client import BlogPost
+from amaraapi import AmaraTools, AmaraVideo
+import traceback
 
 class BlogRepository:
 
-    def __init__(self, mongo_connection, blogId):
+    def __init__(self, mongo_connection, blogId, amara_headers):
         self.mongo_connect = mongo_connection
         self.client = MongoClient(mongo_connection)
         self.musicblogs_database = self.client.musicblogs
@@ -20,6 +22,9 @@ class BlogRepository:
             }
 
         self.postids = set(self.posts_map.keys())
+        self.amara_headers = amara_headers
+        self.amara_tools = AmaraTools(self.amara_headers)
+        self.subtitles_collection = self.musicblogs_database['subtitles.' + blogId]
 
     def update_blog_post(self, blog_post):
         if not blog_post:
@@ -27,9 +32,10 @@ class BlogRepository:
 
         if not hasattr(blog_post, "postId"):
             return
-        if (blog_post.postId in self.postids):
+        if blog_post.postId in self.postids:
             self.postids.remove(blog_post.postId)
-        if blog_post.postId in self.posts_map :
+
+        if blog_post.postId in self.posts_map:
 
             update_key, update_value = {'postId': blog_post.postId}, {k: v for k, v in blog_post._asdict().items() if k not in "postId"}
 
@@ -44,6 +50,36 @@ class BlogRepository:
             print("inserting {} ".format(blog_post.postId))
             self.posts_collection.insert_one(blog_post._asdict())
             print("inserted {} ".format(blog_post.postId))
+
+    def update_sub_titles(self, blog_post, languages):
+#
+        if hasattr(blog_post, "labels"):
+            labels = blog_post.labels
+            video_url = blog_post.videoId
+
+            if labels and ('subtitled' in labels or 'SUBTITLED' in labels):
+                print("Trying to get video for {}".format(video_url))
+                try:
+                    amara_id = self.amara_tools.get_video_id(video_url='https://youtu.be/' + video_url)
+                    amara_video = AmaraVideo(self.amara_headers, amara_id)
+                    if amara_video:
+                        languages_video = amara_video.get_languages()
+                        common_languages = [l for l in languages_video if l in languages]
+                        if common_languages:
+                            sel_language = common_languages[0]
+                            subtitles = amara_video.get_subtitles(sel_language)
+                            if subtitles and len(subtitles) > 0:
+                                print("Saving subtitles for {}".format(video_url))
+                                self.subtitles_collection.replace_one(
+                                    filter={"video_url": video_url},
+                                    replacement={"video_url": video_url, "video_id": amara_id, "lang": sel_language,
+                                                 "subtitles": subtitles},
+                                    upsert=True
+                                )
+
+                except:
+                    print("Could not process {} from {}".format(video_url, blog_post.url))
+                    traceback.print_exc()
 
     def delete_old_posts(self):
         for postid in self.postids:
